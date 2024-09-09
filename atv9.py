95efa97482586938ce649822696b6fae677c3b4f
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-09 09:41:34 -0300

    first commit

---
+++ atv9.py
@@ -0,0 +1,5 @@
+# Um motorista deseja calcular o consumo médio de combustível do seu carro. Crie um programa que receba a quantidade de quilômetros percorridos e a quantidade de litros de combustível consumidos, e calcule o consumo médio (km por litro).
+km = int(input("quantos km vc percoreu"))
+lt = int(input("quantos litros vc usou"))
+consumo = km/lt
+print(consumo)
